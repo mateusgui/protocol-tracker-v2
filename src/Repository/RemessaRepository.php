21c92c82a8a6d62129761ba9299e505c761c1292
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-06 16:01:53 -0400

    Update RemessaRepository.php

---
+++ src/Repository/RemessaRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Mateus\ProtocolTrackerV2\Repository;
+
+use Mateus\ProtocolTrackerV2\Interfaces\RemessaRepositoryInterface;
+
+class RemessaRepository implements RemessaRepositoryInterface
+{
+
+}
