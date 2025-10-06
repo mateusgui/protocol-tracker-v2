89ae3d148b8d7117907eb0a599247fb9b286b6b0
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-06 16:21:45 -0400

    Update RemessaRepository.php

--- src/Repository/RemessaRepository.php
+++ src/Repository/RemessaRepository.php
@@ -2,9 +2,41 @@
 
 namespace Mateus\ProtocolTrackerV2\Repository;
 
+use DateTimeImmutable;
 use Mateus\ProtocolTrackerV2\Interfaces\RemessaRepositoryInterface;
+use Mateus\ProtocolTrackerV2\Model\Remessa;
+use PDO;
 
 class RemessaRepository implements RemessaRepositoryInterface
 {
+    private PDO $connection;
 
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function all(): array
+    {
+        return [];
+    }
+
+    public function search(?string $numero = null, ?DateTimeImmutable $dataInicio = null, ?DateTimeImmutable $dataFim = null): array
+    {
+        return [];
+    }
+
+    public function add(Remessa $remessa): void
+    {
+
+    }
+
+    public function update(Remessa $remessa): void
+    {
+
+    }
+    public function delete($id): void
+    {
+
+    }
 }
