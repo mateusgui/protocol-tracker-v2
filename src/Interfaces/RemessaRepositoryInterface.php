41b73573318fa33e3740ff34fa1450c149606c83
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-06 16:19:27 -0400

    Update RemessaRepositoryInterface.php

--- src/Interfaces/RemessaRepositoryInterface.php
+++ src/Interfaces/RemessaRepositoryInterface.php
@@ -2,7 +2,14 @@
 
 namespace Mateus\ProtocolTrackerV2\Interfaces;
 
+use DateTimeImmutable;
+use Mateus\ProtocolTrackerV2\Model\Remessa;
+
 interface RemessaRepositoryInterface
 {
-    
+    public function all(): array;
+    public function search(?string $numero = null, ?DateTimeImmutable $dataInicio = null, ?DateTimeImmutable $dataFim = null): array;
+    public function add(Remessa $remessa): void;
+    public function update(Remessa $remessa): void;
+    public function delete($id): void;
 }
