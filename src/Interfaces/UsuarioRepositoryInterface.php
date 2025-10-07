7e298137411f568f6128f77a29c98903792792b7
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-07 10:25:17 -0400

    Update UsuarioRepositoryInterface.php

--- src/Interfaces/UsuarioRepositoryInterface.php
+++ src/Interfaces/UsuarioRepositoryInterface.php
@@ -7,6 +7,7 @@ use Mateus\ProtocolTrackerV2\Model\Usuario;
 interface UsuarioRepositoryInterface
 {
     public function all(): array;
+    public function findById(int $id): ?Usuario;
     public function add(Usuario $usuario): void;
     public function update(Usuario $usuario): void;
     public function alteraSenha(int $id, string $hash_senha): void;
