9f2bdcb2c1e56a6ad52019de40dd601284eeb139
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-07 10:57:45 -0400

    Update RemessaService.php

--- src/Service/RemessaService.php
+++ src/Service/RemessaService.php
@@ -2,8 +2,13 @@
 
 namespace Mateus\ProtocolTrackerV2\Service;
 
+use DateTimeImmutable;
+use Exception;
 use Mateus\ProtocolTrackerV2\Interfaces\RemessaRepositoryInterface;
 use Mateus\ProtocolTrackerV2\Interfaces\UsuarioRepositoryInterface;
+use Mateus\ProtocolTrackerV2\Model\Remessa;
+use Mateus\ProtocolTrackerV2\Model\Usuario;
+use Ramsey\Uuid\Uuid;
 
 class RemessaService
 {
@@ -12,17 +17,43 @@ class RemessaService
         private UsuarioRepositoryInterface $usuarioRepository
     ) {}
 
-    public function add($data_recebimento, $id_administrador): void
+    public function add(Usuario $usuarioLogado, string $data_recebimento): void
     {
-        //$sqlQuery = "INSERT INTO remessas (id, data_recebimento, id_administrador) VALUES (:id, :data_recebimento, :id_administrador);";
+        if ($usuarioLogado->getPermissao() !== 'administrador') {
+            throw new Exception("Apenas administradores podem criar novas remessas.");
+        }
 
+        try {
+            $data_recebimento = new DateTimeImmutable($data_recebimento);
+        } catch (Exception $e) {
+            throw new Exception("Falha ao criar data");
+        }
 
+        $id = Uuid::uuid4()->toString();
 
+        $remessa = new Remessa(
+            $id,
+            $data_recebimento,
+            null,
+            'RECEBIDO',
+            0,
+            $usuarioLogado->getId(),
+            null
+        );
+
+        $this->remessaRepository->add($remessa);
     }
 
-    public function update(): void
+    public function update(Usuario $usuarioLogado, string $data_recebimento, string $data_entrega, string $status, string $observacoes, string $id): void
     {
-        //$sqlQuery = "UPDATE remessas SET data_recebimento = :data_recebimento, data_entrega = :data_entrega, status = :status, quantidade_protocolos = :quantidade_protocolos, observacoes = :observacoes WHERE id = :id;";
+        //$sqlQuery = "UPDATE remessas SET data_recebimento = :data_recebimento, data_entrega = :data_entrega, status = :status, observacoes = :observacoes WHERE id = :id;";
+        if ($usuarioLogado->getPermissao() !== 'administrador') {
+            throw new Exception("Apenas administradores podem editar remessas.");
+        }
+        //Validar se remessa existe pelo ID
+        //Validar se data recebimento < data entrega
+        //Validar status
+        //Implementar função que adiciona +1 quantidade_protocolos
 
 
 
