b7b1bc12fb62c07c74b81fa5d1e99caa02326c84
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-07 10:15:24 -0400

    Update RemessaService.php

---
+++ src/Service/RemessaService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mateus\ProtocolTrackerV2\Service;
+
+use Mateus\ProtocolTrackerV2\Interfaces\RemessaRepositoryInterface;
+
+class RemessaService
+{
+    public function __construct(
+        private RemessaRepositoryInterface $remessaRepository
+    ) {}
+
+    public function add($data_recebimento, $id_administrador): void
+    {
+        //$sqlQuery = "INSERT INTO remessas (id, data_recebimento, id_administrador) VALUES (:id, :data_recebimento, :id_administrador);";
+
+        
+
+    }
+
+    public function update(): void
+    {
+        //$sqlQuery = "UPDATE remessas SET data_recebimento = :data_recebimento, data_entrega = :data_entrega, status = :status, quantidade_protocolos = :quantidade_protocolos, observacoes = :observacoes WHERE id = :id;";
+
+
+
+    }
+}
+
+/**public function __construct(
+        private readonly string $id,
+        private readonly ?DateTimeImmutable $data_recebimento,
+        private readonly ?DateTimeImmutable $data_entrega,
+        private readonly string $status,
+        private readonly ?int $quantidade_protocolos,
+        private readonly int $id_administrador,
+        private readonly ?string $observacoes
+        )
+        {} */
