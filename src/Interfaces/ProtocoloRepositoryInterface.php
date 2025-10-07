774a3532adac950ff9097cd6dfabf8e6547c42da
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-06 21:11:36 -0400

    Update ProtocoloRepositoryInterface.php

--- src/Interfaces/ProtocoloRepositoryInterface.php
+++ src/Interfaces/ProtocoloRepositoryInterface.php
@@ -2,7 +2,12 @@
 
 namespace Mateus\ProtocolTrackerV2\Interfaces;
 
+use Mateus\ProtocolTrackerV2\Model\Protocolo;
+
 interface ProtocoloRepositoryInterface
 {
-    
+    public function all(): array;
+    public function add(Protocolo $protocolo): void;
+    public function update(Protocolo $protocolo): void;
+    public function movimentarProtocolo(string $id, string $status): void;
 }
