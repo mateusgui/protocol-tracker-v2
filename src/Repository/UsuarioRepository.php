a28cb407d2bceb7e800e410d94e047d709d6e55f
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-07 09:21:01 -0400

    Update UsuarioRepository.php

--- src/Repository/UsuarioRepository.php
+++ src/Repository/UsuarioRepository.php
@@ -3,8 +3,80 @@
 namespace Mateus\ProtocolTrackerV2\Repository;
 
 use Mateus\ProtocolTrackerV2\Interfaces\UsuarioRepositoryInterface;
+use Mateus\ProtocolTrackerV2\Model\Usuario;
+use PDO;
+use PDOStatement;
 
 class UsuarioRepository implements UsuarioRepositoryInterface
 {
+    private PDO $connection;
 
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function all(): array
+    {
+        $sqlQuery = "SELECT * FROM usuarios ORDER BY data_Criacao DESC;";
+
+        $stmt = $this->connection->query($sqlQuery);
+
+        return $this->hidrataLista($stmt);
+    }
+
+    public function add(Usuario $usuario): void
+    {
+        $sqlQuery = "INSERT INTO usuarios (nome, email, cpf, hash_senha, permissao) VALUES (:nome, :email, :cpf, :hash_senha, :permissao);";
+
+        $usuarioArray = $usuario->toArray();
+
+        $stmt = $this->connection->prepare($sqlQuery);
+        $stmt->bindValue(':nome', $usuarioArray['nome']);
+        $stmt->bindValue(':email', $usuarioArray['email']);
+        $stmt->bindValue(':cpf', $usuarioArray['cpf']);
+        $stmt->bindValue(':hash_senha', $usuarioArray['hash_senha']);
+        $stmt->bindValue(':permissao', $usuarioArray['permissao']);
+
+        $stmt->execute();
+    }
+
+    public function update(Usuario $usuario): void
+    {
+        $sqlQuery = "UPDATE usuarios SET nome = :nome, email = :email, cpf = :cpf, permissao = :permissao, status = :status WHERE id = :id;";
+
+        $usuarioArray = $usuario->toArray();
+
+        $stmt = $this->connection->prepare($sqlQuery);
+        $stmt->bindValue(':nome', $usuarioArray['nome']);
+        $stmt->bindValue(':email', $usuarioArray['email']);
+        $stmt->bindValue(':cpf', $usuarioArray['cpf']);
+        $stmt->bindValue(':permissao', $usuarioArray['permissao']);
+        $stmt->bindValue(':status', $usuarioArray['status']);
+        $stmt->bindValue(':id', $usuarioArray['id']);
+
+        $stmt->execute();
+    }
+
+    public function alteraSenha(int $id, string $hash_senha): void
+    {
+        $sqlQuery = "UPDATE usuarios SET hash_senha = :hash_senha WHERE id = :id;";
+
+        $stmt = $this->connection->prepare($sqlQuery);
+        $stmt->bindValue(':hash_senha', $hash_senha);
+        $stmt->bindValue(':id', $id);
+
+        $stmt->execute();
+    }
+
+    private function hidrataLista(PDOStatement $stmt): array
+    {
+        $listaDeUsuarios = [];
+
+        while($dadosUsuario = $stmt->fetch()){
+            $listaDeUsuarios[] = Usuario::fromArray($dadosUsuario);
+        }
+
+        return $listaDeUsuarios;
+    }
 }
