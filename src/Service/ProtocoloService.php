7a8b5fb7cf1ab6e4eebed1fe72f5420492a2b4fc
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-07 14:15:29 -0400

    Update ProtocoloService.php

--- src/Service/ProtocoloService.php
+++ src/Service/ProtocoloService.php
@@ -2,10 +2,13 @@
 
 namespace Mateus\ProtocolTrackerV2\Service;
 
+use Exception;
 use Mateus\ProtocolTrackerV2\Interfaces\ProtocoloRepositoryInterface;
 use Mateus\ProtocolTrackerV2\Interfaces\RemessaRepositoryInterface;
 use Mateus\ProtocolTrackerV2\Interfaces\UsuarioRepositoryInterface;
 use Mateus\ProtocolTrackerV2\Model\Protocolo;
+use Mateus\ProtocolTrackerV2\Model\Usuario;
+use Ramsey\Uuid\Uuid;
 
 class ProtocoloService
 {
@@ -15,12 +18,45 @@ class ProtocoloService
         private UsuarioRepositoryInterface $usuarioRepository
     ) {}
 
-    public function add(Protocolo $protocolo): void
+    public function novoProtocolo(Usuario $usuarioLogado, string $id_remessa, string $numero_protocolo): void
     {
+        if($usuarioLogado->getPermissao() !== 'administrador') {
+            throw new Exception("Apenas administradores podem cadastrar novos protocolos");
+        }
+
+        $remessa = $this->remessaRepository->findById($id_remessa);
+        if(empty($remessa)){
+            throw new Exception("Remessa informada não foi localizada");
+        }
+
+        if(strlen($numero_protocolo) !== 6 || !ctype_digit($numero_protocolo)){
+            throw new Exception("O número do protocolo precisa ter exatamente 6 dígitos e possuir somente números");
+        }
+
+        if(!is_null($this->protocoloRepository->findByNumber($numero_protocolo))){
+            throw new Exception("O número do protocolo informado já foi registrado");
+        }
+
+        $id = Uuid::uuid4()->toString();
+
+        $protocolo = new Protocolo(
+            $id,
+            $id_remessa,
+            $numero_protocolo,
+            null,
+            null,
+            null,
+            null,
+            'RECEBIDO',
+            null,
+            null
+        );
+
+        $this->protocoloRepository->add($protocolo);
 
     }
 
-    public function update(Protocolo $protocolo): void
+    public function atualizarProtocolo(Usuario $usuarioLogado): void
     {
 
     }
