fbbe065b54d8803b2e5b664851ac95a128f3168d
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-10-07 20:03:44 -0400

    Update LoginService.php

---
+++ src/Service/LoginService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mateus\ProtocolTrackerV2\Service;
+
+use Exception;
+use Mateus\ProtocolTrackerV2\Interfaces\UsuarioRepositoryInterface;
+
+class LoginService
+{
+    public function __construct(
+        private UsuarioRepositoryInterface $usuarioRepository
+    ) {}
+
+    /**
+     * Realiza validações para que um usuário possa fazer login
+     * @throws Exception Se os dados forem inválidos.
+     * @param string $cpf CPF informado pelo Usuario
+     * @param string $senha senha informada pelo Usuario
+     * @return void Array associativo
+     */
+    public function login(string $cpf, string $senha): void
+    {
+        $usuario = $this->usuarioRepository->findByCpf($cpf);
+
+        if(!$usuario->getStatus()){
+            throw new Exception("Usuário inativo");
+        }
+
+        $senhaCorreta = password_verify($senha, $usuario?->getHashSenha() ?? '');
+
+        if ($usuario === null || !$senhaCorreta) {
+            throw new Exception("CPF ou senha inválidos.");
+        }
+
+        session_regenerate_id(true);
+
+        $_SESSION['usuario_logado_id'] = $usuario->getId();
+    }
+}
